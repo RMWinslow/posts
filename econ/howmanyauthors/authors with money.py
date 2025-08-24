63ed2130bfa8a766341ce26e7be54f3fe5ed47ce
RMWinslow <robertmartinwinslowks@gmail.com>
2025-08-24 10:20:37 -0500

    Calculate how many authors are earning >20k (~45k)
    Here are the top
    
    Total	61340	121799	183139
    ENT-Independent Artists, Writers, And Performers	37612	7522	45134
    PRF-Advertising, Public Relations, And Related Services	3465	13012	16477
    INF-Periodical, Book, And Directory Publishers	3658	4934	8592
    EDU-Colleges, Universities, And Professional Schools, Including Junior Colleges	87	8280	8367
    PRF-Management, Scientific, And Technical Consulting Services	3212	4689	7901
    SRV-Civic, Social, Advocacy Organizations, And Grantmaking And Giving Services	237	7334	7571
    INF-Motion Pictures And Video Industries	3053	4161	7214
    PRF-Computer Systems Design And Related Services	629	5289	5918
    INF-Internet Publishing And Broadcasting And Web Search Portals	3099	2807	5906
    INF-Broadcasting (Except Internet)	748	3174	3922

--- econ/howmanyauthors/authors with money.py
+++ econ/howmanyauthors/authors with money.py
@@ -5,8 +5,7 @@ import csv
 
 df = pd.read_stata("usa_00014.dta")
 
-#%% CODEBOOK FOR INDUSTRIES
-
+#%% PROCESSING AND HELPERS
 # codebook for industries
 ind_code_to_desc = {}
 with open('US2022C_INDP.csv', 'r') as file:
@@ -14,13 +13,48 @@ with open('US2022C_INDP.csv', 'r') as file:
     next(reader)  # skip header
     for row in reader:
         ind_code_to_desc[int(row[0])] = row[1]
+        ind_code_to_desc[row[0]] = row[1]
 
-# apply codebook 
+print("missing codes: ", set(df['us2022c_indp'].unique()) - set(ind_code_to_desc.keys()))
+
+##%% apply codebook 
 df['ind desc'] = df['us2022c_indp'].map(ind_code_to_desc)
 df.head(20)
+
+##%% Define format for results table
+# It's a weighted crosstab, sorted by total counts
+def crosstab(df, rowvar, colvar, weightvar='perwt'):
+    crosstab = pd.crosstab(df[rowvar],df[colvar],values=df[weightvar],aggfunc='sum')
+    crosstab['Total'] = crosstab.sum(axis=1)
+    crosstab.loc['Total'] = crosstab.sum(axis=0)
+    return crosstab.sort_values(by='Total', ascending=False, axis=0)
+
+
+
+#%% FILTERING
+# Restrict to "authors and writers" occupation code 2850
+# df[df['occ'] == 2850]['occ1990'].value_counts() # (Interesting. Nine "military" occ1990 recodes.)
+authors = df[df['occ'] == 2850]
+
+# restrict to those with the "independent artists, writers, and performers" industry code 8564
+# authors = authors[authors['us2022c_indp'] == '8564']
+
+#restrict to those with personal income > 20k 
+# (inctot is total personal income, incearn is personal income from earnings)
+# I think incearn is better, since inctot incluedes 
+authors = authors[authors['incearn'] >= 20000]
+# authors = authors[authors['inctot'] > 20000]
+# authors = authors[authors['inctot'] < 9999998] # remove unknowns and N/As. Should be redundant with incearn filter above
+# authors = authors[authors['incwage'] >= 20000]
+
+# authors = authors[authors['empstat']=='employed']
+
+
+## %% RESULTS
+crosstab(authors, 'ind desc', 'classwkr', 'perwt').drop('n/a',axis=1).head(11)
+# I return 11 for top ten + total row
 
 
 
 # %%
 
-df
